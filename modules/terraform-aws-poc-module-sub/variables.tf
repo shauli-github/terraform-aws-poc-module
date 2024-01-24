0f67fab13ac81d8d48b12aa259a811904d1cacf5
shauli.riger <shauli.riger@imperva.com>
2024-01-24 14:21:07 +0200

    Test

---
+++ modules/terraform-aws-poc-module-sub/variables.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# Input variable definitions
+
+variable "bucket_name" {
+  description = "Name of the s3 bucket. Must be unique."
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to set on the bucket."
+  type        = map(string)
+  default     = {}
+}
+
+variable "test" {
+  description = "Test description"
+  type        = string
+  default     = "Test description"
+}
